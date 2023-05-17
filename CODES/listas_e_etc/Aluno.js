a42e1ff77f1b6abae1ac7265ccfd9f12704e493f
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2023-05-17 20:28:45 -0300

    iniciei listas e criei um objeto aluno na aula de estruturas de dados

---
+++ CODES/listas_e_etc/Aluno.js
@@ -0,0 +1,18 @@
+class Aluno{
+    constructor(){
+        this.numero=0
+        this.nome=""
+        this.a1=0
+        this.a2=0
+        this.af=0
+        this.media=0
+    }
+}
+
+
+
+var aluno=new Aluno()
+aluno.numero=1
+aluno.nome="Aecio"
+aluno.a1=9
+console.log(aluno)
