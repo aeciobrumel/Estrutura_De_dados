842f80b13034d822849efeff7d802ef6c1674643
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2023-05-17 21:55:32 -0300

    de classe e objeto com suas  funções

--- CODES/listas_e_etc/Aluno.js
+++ CODES/listas_e_etc/Aluno.js
@@ -1,18 +1,72 @@
 class Aluno{
+    constructor(numero,nome){
+        this.numero=numero
+        this.nome=nome
+        this.notas=new Notas()
+    }
+} 
+class Notas{
     constructor(){
-        this.numero=0
-        this.nome=""
         this.a1=0
         this.a2=0
-        this.af=0
-        this.media=0
+        this.af=null
+        
     }
+    media() {
+        if(this.af==null){
+       return this.a1+this.a2
+        }else{
+            return this.a1<this.a2?this.af+this.a2:this.a1+this.af;
+        }
+
+    }
+    resultado(){
+        return this.media()>=6?"Aprovado":"Reprovado";
+    }
+
+    //teste
+    check(nota){
+        if(nota>5)return 5
+        if(nota<0)return 0
+        return nota
+    }
+
+
+//passa as notas pelo teste
+    setA1(nota){
+        this.a1 = this.check(nota)
+    }
+    setA2(nota){
+        this.a2 = this.check(nota)
+    }
+    setAf(nota){
+        this.af = this.check(nota)
+    }
+
+
 }
+/*
+//list
+var alunos=[
+new Aluno(1,"aecio"),
+new Aluno(2,"nicoly"),
+new Aluno(3,"elamario"),
+new Aluno(4,"Jonas")
+]
 
 
+*/
 
-var aluno=new Aluno()
-aluno.numero=1
-aluno.nome="Aecio"
-aluno.a1= 9
-console.log(aluno)
+
+//obj alunos
+
+var aluno  =new Aluno(1,"elmario")
+aluno.notas.setA1(4)
+aluno.notas.setA2(-2)
+aluno.notas.setAf(7)
+
+//print object
+console.clear()
+console.log(aluno);
+console.log("media: "+aluno.notas.media());
+console.log("Resultado: ",aluno.notas.resultado());aluno.notas.setA1=4
